f8ecb629ca90e08dc41d270b32af2ec0ddcc5593
bronku <bronk.jakub@proton.me>
2025-04-16 19:59:11 +0200

    extracted datasource to settings

--- server/Data/ApplicationDbContext.cs
+++ server/Data/ApplicationDbContext.cs
@@ -3,20 +3,18 @@ using server.Models;
 
 namespace server.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
 {
     public required DbSet<Contact> Contacts { get; set; }
     public required DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        if (!options.IsConfigured) options.UseSqlite("Data Source=database.db");
+        if (!options.IsConfigured) options.UseSqlite($"Data Source={configuration["Sqlite:file"]}");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Contact>().Property(u => u.Category).HasConversion<string>();
-        modelBuilder.Entity<Contact>().Property(u => u.BusinessCategory).HasConversion<string>();
         modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
 
         base.OnModelCreating(modelBuilder);

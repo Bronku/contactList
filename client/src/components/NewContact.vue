59e10136d0b79ad73265f9c85df59a0b8b2a6b45
bronku <bronk.jakub@proton.me>
2025-04-17 22:25:08 +0200

    cleanup

--- client/src/components/NewContact.vue
+++ client/src/components/NewContact.vue
@@ -1,23 +1,17 @@
 <script lang="ts" setup>
-import {contactStorage} from "@/storage/contactStorage.ts";
-import {type Ref, ref} from "vue";
-import ContactEditor from "@/components/ContactEditor.vue";
-import type {contactType} from "@/types/contact.ts";
+import { contactStorage } from '@/storage/contactStorage.ts'
+import { type Ref, ref } from 'vue'
+import ContactEditor from '@/components/ContactEditor.vue'
+import type { contactType } from '@/types/contact.ts'
 
-const editedContact: Ref<contactType | null> = ref(null);
+const editedContact: Ref<contactType | null> = ref(null)
 
 function openEditor() {
-    editedContact.value = contactStorage.newContact();
+    contactStorage.newContact()
 }
-
-function closeEditor() {
-    editedContact.value = null;
-}
-
 </script>
 
 <template>
     <button v-if="!editedContact" @click.prevent="openEditor">new</button>
-    <ContactEditor v-if="editedContact" :contact="editedContact" :editing="false"
-                   @close="closeEditor"/>
+    <ContactEditor v-if="editedContact" />
 </template>
